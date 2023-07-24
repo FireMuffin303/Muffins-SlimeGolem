4d07f62e46c7adddbf1ad4f8b77ec8ca63810736
FireMuffin303 <firegame303@gmail.com>
2023-07-24 16:50:10 +0700

    datagen StoneCutter Recipe

--- fabric/src/main/java/net/firemuffin303/slimegolem/fabric/datagen/provider/ModRecipeProvider.java
+++ fabric/src/main/java/net/firemuffin303/slimegolem/fabric/datagen/provider/ModRecipeProvider.java
@@ -65,93 +65,191 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 ModBlock.PACKED_SLIME_BRICK_STAIR.get(),
                 ModBlock.PACKED_SLIME_BRICK_WALL.get()});*/
 
-        stonecut(ModItem.PACKED_SLIME_BLOCK,ModItem.PACKED_SLIME_BRICKS,1,exporter);
-        stonecut(ModItem.PACKED_SLIME_BLOCK,ModItem.CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.PACKED_SLIME_BLOCK,ModItem.PACKED_SLIME_SLAB,2,exporter);
-        stonecut(ModItem.PACKED_SLIME_BLOCK,ModItem.PACKED_SLIME_STAIR,1,exporter);
-        stonecut(ModItem.PACKED_SLIME_BLOCK,ModItem.PACKED_SLIME_WALL,1,exporter);
-        stonecut(ModItem.PACKED_SLIME_BLOCK,ModItem.PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.PACKED_SLIME_BLOCK,ModItem.PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.PACKED_SLIME_BLOCK,ModItem.PACKED_SLIME_BRICK_WALL,1,exporter);
-
-        stonecut(ModItem.PACKED_SLIME_BRICKS,ModItem.CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.PACKED_SLIME_BRICKS,ModItem.PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.PACKED_SLIME_BRICKS,ModItem.PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.PACKED_SLIME_BRICKS,ModItem.PACKED_SLIME_BRICK_WALL,1,exporter);
+        stoneCutFamily(ModItem.PACKED_SLIME_BLOCK,
+                ModItem.PACKED_SLIME_BRICKS,
+                ModItem.CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.PACKED_SLIME_SLAB,
+                ModItem.PACKED_SLIME_STAIR,
+                ModItem.PACKED_SLIME_WALL,
+                ModItem.PACKED_SLIME_BRICK_SLAB,
+                ModItem.PACKED_SLIME_BRICK_STAIR,
+                ModItem.PACKED_SLIME_BRICK_WALL,exporter);
 
 
-        stonecut(ModItem.WHITE_PACKED_SLIME_BLOCK,ModItem.WHITE_PACKED_SLIME_BRICKS,1,exporter);
-        stonecut(ModItem.WHITE_PACKED_SLIME_BLOCK,ModItem.WHITE_CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.WHITE_PACKED_SLIME_BLOCK,ModItem.WHITE_PACKED_SLIME_SLAB,2,exporter);
-        stonecut(ModItem.WHITE_PACKED_SLIME_BLOCK,ModItem.WHITE_PACKED_SLIME_STAIR,1,exporter);
-        stonecut(ModItem.WHITE_PACKED_SLIME_BLOCK,ModItem.WHITE_PACKED_SLIME_WALL,1,exporter);
-        stonecut(ModItem.WHITE_PACKED_SLIME_BLOCK,ModItem.WHITE_PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.WHITE_PACKED_SLIME_BLOCK,ModItem.WHITE_PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.WHITE_PACKED_SLIME_BLOCK,ModItem.WHITE_PACKED_SLIME_BRICK_WALL,1,exporter);
+        stoneCutFamily(ModItem.WHITE_PACKED_SLIME_BLOCK,
+                ModItem.WHITE_PACKED_SLIME_BRICKS,
+                ModItem.WHITE_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.WHITE_PACKED_SLIME_SLAB,
+                ModItem.WHITE_PACKED_SLIME_STAIR,
+                ModItem.WHITE_PACKED_SLIME_WALL,
+                ModItem.WHITE_PACKED_SLIME_BRICK_SLAB,
+                ModItem.WHITE_PACKED_SLIME_BRICK_STAIR,
+                ModItem.WHITE_PACKED_SLIME_BRICK_WALL,exporter);
 
-        stonecut(ModItem.WHITE_PACKED_SLIME_BRICKS,ModItem.WHITE_CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.WHITE_PACKED_SLIME_BRICKS,ModItem.WHITE_PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.WHITE_PACKED_SLIME_BRICKS,ModItem.WHITE_PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.WHITE_PACKED_SLIME_BRICKS,ModItem.WHITE_PACKED_SLIME_BRICK_WALL,1,exporter);
+        stoneCutFamily(
+                ModItem.LIGHT_GRAY_PACKED_SLIME_BLOCK,
+                ModItem.LIGHT_GRAY_PACKED_SLIME_BRICKS,
+                ModItem.LIGHT_GRAY_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.LIGHT_GRAY_PACKED_SLIME_SLAB,
+                ModItem.LIGHT_GRAY_PACKED_SLIME_STAIR,
+                ModItem.LIGHT_GRAY_PACKED_SLIME_WALL,
+                ModItem.LIGHT_GRAY_PACKED_SLIME_BRICK_SLAB,
+                ModItem.LIGHT_GRAY_PACKED_SLIME_BRICK_STAIR,
+                ModItem.LIGHT_GRAY_PACKED_SLIME_BRICK_WALL,exporter);
 
+        stoneCutFamily(
+                ModItem.GRAY_PACKED_SLIME_BLOCK,
+                ModItem.GRAY_PACKED_SLIME_BRICKS,
+                ModItem.GRAY_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.GRAY_PACKED_SLIME_SLAB,
+                ModItem.GRAY_PACKED_SLIME_STAIR,
+                ModItem.GRAY_PACKED_SLIME_WALL,
+                ModItem.GRAY_PACKED_SLIME_BRICK_SLAB,
+                ModItem.GRAY_PACKED_SLIME_BRICK_STAIR,
+                ModItem.GRAY_PACKED_SLIME_BRICK_WALL,exporter);
 
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BLOCK,ModItem.LIGHT_GRAY_PACKED_SLIME_BRICKS,1,exporter);
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BLOCK,ModItem.LIGHT_GRAY_CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BLOCK,ModItem.LIGHT_GRAY_PACKED_SLIME_SLAB,2,exporter);
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BLOCK,ModItem.LIGHT_GRAY_PACKED_SLIME_STAIR,1,exporter);
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BLOCK,ModItem.LIGHT_GRAY_PACKED_SLIME_WALL,1,exporter);
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BLOCK,ModItem.LIGHT_GRAY_PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BLOCK,ModItem.LIGHT_GRAY_PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BLOCK,ModItem.LIGHT_GRAY_PACKED_SLIME_BRICK_WALL,1,exporter);
+        stoneCutFamily(
+                ModItem.BLACK_PACKED_SLIME_BLOCK,
+                ModItem.BLACK_PACKED_SLIME_BRICKS,
+                ModItem.BLACK_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.BLACK_PACKED_SLIME_SLAB,
+                ModItem.BLACK_PACKED_SLIME_STAIR,
+                ModItem.BLACK_PACKED_SLIME_WALL,
+                ModItem.BLACK_PACKED_SLIME_BRICK_SLAB,
+                ModItem.BLACK_PACKED_SLIME_BRICK_STAIR,
+                ModItem.BLACK_PACKED_SLIME_BRICK_WALL,exporter);
 
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BRICKS,ModItem.LIGHT_GRAY_CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BRICKS,ModItem.LIGHT_GRAY_PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BRICKS,ModItem.LIGHT_GRAY_PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.LIGHT_GRAY_PACKED_SLIME_BRICKS,ModItem.LIGHT_GRAY_PACKED_SLIME_BRICK_WALL,1,exporter);
+        stoneCutFamily(
+                ModItem.BROWN_PACKED_SLIME_BLOCK,
+                ModItem.BROWN_PACKED_SLIME_BRICKS,
+                ModItem.BROWN_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.BROWN_PACKED_SLIME_SLAB,
+                ModItem.BROWN_PACKED_SLIME_STAIR,
+                ModItem.BROWN_PACKED_SLIME_WALL,
+                ModItem.BROWN_PACKED_SLIME_BRICK_SLAB,
+                ModItem.BROWN_PACKED_SLIME_BRICK_STAIR,
+                ModItem.BROWN_PACKED_SLIME_BRICK_WALL,exporter);
 
-        stonecut(ModItem.GRAY_PACKED_SLIME_BLOCK,ModItem.GRAY_PACKED_SLIME_BRICKS,1,exporter);
-        stonecut(ModItem.GRAY_PACKED_SLIME_BLOCK,ModItem.GRAY_CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.GRAY_PACKED_SLIME_BLOCK,ModItem.GRAY_PACKED_SLIME_SLAB,2,exporter);
-        stonecut(ModItem.GRAY_PACKED_SLIME_BLOCK,ModItem.GRAY_PACKED_SLIME_STAIR,1,exporter);
-        stonecut(ModItem.GRAY_PACKED_SLIME_BLOCK,ModItem.GRAY_PACKED_SLIME_WALL,1,exporter);
-        stonecut(ModItem.GRAY_PACKED_SLIME_BLOCK,ModItem.GRAY_PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.GRAY_PACKED_SLIME_BLOCK,ModItem.GRAY_PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.GRAY_PACKED_SLIME_BLOCK,ModItem.GRAY_PACKED_SLIME_BRICK_WALL,1,exporter);
+        stoneCutFamily(
+                ModItem.RED_PACKED_SLIME_BLOCK,
+                ModItem.RED_PACKED_SLIME_BRICKS,
+                ModItem.RED_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.RED_PACKED_SLIME_SLAB,
+                ModItem.RED_PACKED_SLIME_STAIR,
+                ModItem.RED_PACKED_SLIME_WALL,
+                ModItem.RED_PACKED_SLIME_BRICK_SLAB,
+                ModItem.RED_PACKED_SLIME_BRICK_STAIR,
+                ModItem.RED_PACKED_SLIME_BRICK_WALL,exporter);
 
-        stonecut(ModItem.GRAY_PACKED_SLIME_BRICKS,ModItem.GRAY_CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.GRAY_PACKED_SLIME_BRICKS,ModItem.GRAY_PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.GRAY_PACKED_SLIME_BRICKS,ModItem.GRAY_PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.GRAY_PACKED_SLIME_BRICKS,ModItem.GRAY_PACKED_SLIME_BRICK_WALL,1,exporter);
+        stoneCutFamily(
+                ModItem.ORANGE_PACKED_SLIME_BLOCK,
+                ModItem.ORANGE_PACKED_SLIME_BRICKS,
+                ModItem.ORANGE_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.ORANGE_PACKED_SLIME_SLAB,
+                ModItem.ORANGE_PACKED_SLIME_STAIR,
+                ModItem.ORANGE_PACKED_SLIME_WALL,
+                ModItem.ORANGE_PACKED_SLIME_BRICK_SLAB,
+                ModItem.ORANGE_PACKED_SLIME_BRICK_STAIR,
+                ModItem.ORANGE_PACKED_SLIME_BRICK_WALL,exporter);
 
+        stoneCutFamily(
+                ModItem.YELLOW_PACKED_SLIME_BLOCK,
+                ModItem.YELLOW_PACKED_SLIME_BRICKS,
+                ModItem.YELLOW_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.YELLOW_PACKED_SLIME_SLAB,
+                ModItem.YELLOW_PACKED_SLIME_STAIR,
+                ModItem.YELLOW_PACKED_SLIME_WALL,
+                ModItem.YELLOW_PACKED_SLIME_BRICK_SLAB,
+                ModItem.YELLOW_PACKED_SLIME_BRICK_STAIR,
+                ModItem.YELLOW_PACKED_SLIME_BRICK_WALL,exporter);
 
-        stonecut(ModItem.BLACK_PACKED_SLIME_BLOCK,ModItem.BLACK_PACKED_SLIME_BRICKS,1,exporter);
-        stonecut(ModItem.BLACK_PACKED_SLIME_BLOCK,ModItem.BLACK_CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.BLACK_PACKED_SLIME_BLOCK,ModItem.BLACK_PACKED_SLIME_SLAB,2,exporter);
-        stonecut(ModItem.BLACK_PACKED_SLIME_BLOCK,ModItem.BLACK_PACKED_SLIME_STAIR,1,exporter);
-        stonecut(ModItem.BLACK_PACKED_SLIME_BLOCK,ModItem.BLACK_PACKED_SLIME_WALL,1,exporter);
-        stonecut(ModItem.BLACK_PACKED_SLIME_BLOCK,ModItem.BLACK_PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.BLACK_PACKED_SLIME_BLOCK,ModItem.BLACK_PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.BLACK_PACKED_SLIME_BLOCK,ModItem.BLACK_PACKED_SLIME_BRICK_WALL,1,exporter);
+        stoneCutFamily(
+                ModItem.LIME_PACKED_SLIME_BLOCK,
+                ModItem.LIME_PACKED_SLIME_BRICKS,
+                ModItem.LIME_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.LIME_PACKED_SLIME_SLAB,
+                ModItem.LIME_PACKED_SLIME_STAIR,
+                ModItem.LIME_PACKED_SLIME_WALL,
+                ModItem.LIME_PACKED_SLIME_BRICK_SLAB,
+                ModItem.LIME_PACKED_SLIME_BRICK_STAIR,
+                ModItem.LIME_PACKED_SLIME_BRICK_WALL,exporter);
 
-        stonecut(ModItem.BLACK_PACKED_SLIME_BRICKS,ModItem.BLACK_CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.BLACK_PACKED_SLIME_BRICKS,ModItem.BLACK_PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.BLACK_PACKED_SLIME_BRICKS,ModItem.BLACK_PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.BLACK_PACKED_SLIME_BRICKS,ModItem.BLACK_PACKED_SLIME_BRICK_WALL,1,exporter);
+        stoneCutFamily(
+                ModItem.GREEN_PACKED_SLIME_BLOCK,
+                ModItem.GREEN_PACKED_SLIME_BRICKS,
+                ModItem.GREEN_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.GREEN_PACKED_SLIME_SLAB,
+                ModItem.GREEN_PACKED_SLIME_STAIR,
+                ModItem.GREEN_PACKED_SLIME_WALL,
+                ModItem.GREEN_PACKED_SLIME_BRICK_SLAB,
+                ModItem.GREEN_PACKED_SLIME_BRICK_STAIR,
+                ModItem.GREEN_PACKED_SLIME_BRICK_WALL,exporter);
 
+        stoneCutFamily(
+                ModItem.CYAN_PACKED_SLIME_BLOCK,
+                ModItem.CYAN_PACKED_SLIME_BRICKS,
+                ModItem.CYAN_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.CYAN_PACKED_SLIME_SLAB,
+                ModItem.CYAN_PACKED_SLIME_STAIR,
+                ModItem.CYAN_PACKED_SLIME_WALL,
+                ModItem.CYAN_PACKED_SLIME_BRICK_SLAB,
+                ModItem.CYAN_PACKED_SLIME_BRICK_STAIR,
+                ModItem.CYAN_PACKED_SLIME_BRICK_WALL,exporter);
 
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BLOCK,ModItem.ORANGE_PACKED_SLIME_BRICKS,1,exporter);
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BLOCK,ModItem.ORANGE_CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BLOCK,ModItem.ORANGE_PACKED_SLIME_SLAB,2,exporter);
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BLOCK,ModItem.ORANGE_PACKED_SLIME_STAIR,1,exporter);
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BLOCK,ModItem.ORANGE_PACKED_SLIME_WALL,1,exporter);
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BLOCK,ModItem.ORANGE_PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BLOCK,ModItem.ORANGE_PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BLOCK,ModItem.ORANGE_PACKED_SLIME_BRICK_WALL,1,exporter);
+        stoneCutFamily(
+                ModItem.LIGHT_BLUE_PACKED_SLIME_BLOCK,
+                ModItem.LIGHT_BLUE_PACKED_SLIME_BRICKS,
+                ModItem.LIGHT_BLUE_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.LIGHT_BLUE_PACKED_SLIME_SLAB,
+                ModItem.LIGHT_BLUE_PACKED_SLIME_STAIR,
+                ModItem.LIGHT_BLUE_PACKED_SLIME_WALL,
+                ModItem.LIGHT_BLUE_PACKED_SLIME_BRICK_SLAB,
+                ModItem.LIGHT_BLUE_PACKED_SLIME_BRICK_STAIR,
+                ModItem.LIGHT_BLUE_PACKED_SLIME_BRICK_WALL,exporter);
 
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BRICKS,ModItem.ORANGE_CHISELED_PACKED_SLIME_BLOCK,1,exporter);
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BRICKS,ModItem.ORANGE_PACKED_SLIME_BRICK_SLAB,2,exporter);
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BRICKS,ModItem.ORANGE_PACKED_SLIME_BRICK_STAIR,1,exporter);
-        stonecut(ModItem.ORANGE_PACKED_SLIME_BRICKS,ModItem.ORANGE_PACKED_SLIME_BRICK_WALL,1,exporter);
+        stoneCutFamily(
+                ModItem.BLUE_PACKED_SLIME_BLOCK,
+                ModItem.BLUE_PACKED_SLIME_BRICKS,
+                ModItem.BLUE_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.BLUE_PACKED_SLIME_SLAB,
+                ModItem.BLUE_PACKED_SLIME_STAIR,
+                ModItem.BLUE_PACKED_SLIME_WALL,
+                ModItem.BLUE_PACKED_SLIME_BRICK_SLAB,
+                ModItem.BLUE_PACKED_SLIME_BRICK_STAIR,
+                ModItem.BLUE_PACKED_SLIME_BRICK_WALL,exporter);
+
+        stoneCutFamily(
+                ModItem.PURPLE_PACKED_SLIME_BLOCK,
+                ModItem.PURPLE_PACKED_SLIME_BRICKS,
+                ModItem.PURPLE_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.PURPLE_PACKED_SLIME_SLAB,
+                ModItem.PURPLE_PACKED_SLIME_STAIR,
+                ModItem.PURPLE_PACKED_SLIME_WALL,
+                ModItem.PURPLE_PACKED_SLIME_BRICK_SLAB,
+                ModItem.PURPLE_PACKED_SLIME_BRICK_STAIR,
+                ModItem.PURPLE_PACKED_SLIME_BRICK_WALL,exporter);
+
+        stoneCutFamily(
+                ModItem.MAGENTA_PACKED_SLIME_BLOCK,
+                ModItem.MAGENTA_PACKED_SLIME_BRICKS,
+                ModItem.MAGENTA_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.MAGENTA_PACKED_SLIME_SLAB,
+                ModItem.MAGENTA_PACKED_SLIME_STAIR,
+                ModItem.MAGENTA_PACKED_SLIME_WALL,
+                ModItem.MAGENTA_PACKED_SLIME_BRICK_SLAB,
+                ModItem.MAGENTA_PACKED_SLIME_BRICK_STAIR,
+                ModItem.MAGENTA_PACKED_SLIME_BRICK_WALL,exporter);
+
+        stoneCutFamily(
+                ModItem.PINK_PACKED_SLIME_BLOCK,
+                ModItem.PINK_PACKED_SLIME_BRICKS,
+                ModItem.PINK_CHISELED_PACKED_SLIME_BLOCK,
+                ModItem.PINK_PACKED_SLIME_SLAB,
+                ModItem.PINK_PACKED_SLIME_STAIR,
+                ModItem.PINK_PACKED_SLIME_WALL,
+                ModItem.PINK_PACKED_SLIME_BRICK_SLAB,
+                ModItem.PINK_PACKED_SLIME_BRICK_STAIR,
+                ModItem.PINK_PACKED_SLIME_BRICK_WALL,exporter);
     }
 
     private void coloredBlock(Item item,Item baseMaterial,Item colorDye, Consumer<FinishedRecipe> consumer){
@@ -166,5 +264,21 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     private void stonecut(RegistryEntry<Item> ingredient, RegistryEntry<Item> result, int i, Consumer<FinishedRecipe> consumer){
         SingleItemRecipeBuilder.stonecutting(Ingredient.of(ingredient.get()),RecipeCategory.DECORATIONS,result.get(),i).unlockedBy(getHasName(ingredient.get()),has(ingredient.get())).save(consumer,result.getId().withSuffix("_from_%s_stonecutting".formatted(ingredient.getId().getPath())));
 
+    }
+
+    private void stoneCutFamily(RegistryEntry<Item> block,RegistryEntry<Item> brick,RegistryEntry<Item> chiseled,RegistryEntry<Item> slab,RegistryEntry<Item> stair,RegistryEntry<Item> wall,RegistryEntry<Item> brick_slab,RegistryEntry<Item> brick_stair,RegistryEntry<Item> brick_wall,Consumer<FinishedRecipe> exporter){
+        stonecut(block,brick,1,exporter);
+        stonecut(block,chiseled,1,exporter);
+        stonecut(block,stair,1,exporter);
+        stonecut(block,slab,2,exporter);
+        stonecut(block,wall,1,exporter);
+        stonecut(block,brick_slab,2,exporter);
+        stonecut(block,brick_stair,1,exporter);
+        stonecut(block,brick_wall,1,exporter);
+
+        stonecut(brick,chiseled,1,exporter);
+        stonecut(brick,brick_slab,2,exporter);
+        stonecut(brick,brick_stair,1,exporter);
+        stonecut(brick,brick_wall,1,exporter);
     }
 }
